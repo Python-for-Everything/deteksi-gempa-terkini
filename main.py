76d65ba7fa14042433c4c979f1467a0477133cc6
Riki Satya Graha <riki.graha@gmail.com>
2024-10-13 09:40:34 +0700

    final config main.py (oct 13rd, 2024)

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 """
 Aplikasi Deteksi Gempa Terkini
-MODULARISASI DENGAN FUNCTIOn
+MODULARISASI DENGAN FUNCTION
 """
 
 
 def ekstraksi_data():
     """
     Tanggal : 03 Oktober 2024
-    Waktu10 : 12:41 WIB
+    Waktu  : 12:41 WIB
     Magnitudo : 3.8
     Kedalaman : 12 km
     Lokasi : LS=2.48 BT=140.43
@@ -24,18 +24,19 @@ def ekstraksi_data():
     hasil['lokasi'] = {'ls':2.48, 'bt':140.43}
     hasil['pusat_gempa'] = 'Pusat gempa berada di darat 12 km timurlaut Kab. Jayapura'
     hasil['keterangan'] = 'Dirasakan (Skala MMI): III Kab. Jayapura'
+
     return hasil
 
 
 def tampilkan_data(result):
     print("Gempa Terakhir Berdasarkan BMKG")
-    print(f"Tanggal'{result['tanggal']}")
-    print(f"Waktu' {result['waktu']}")
-    print(f"Magnitudo' {result['magnitudo']}")
-    print(f"Kedalaman' {result['kedalaman']}")
-    print(f"Lokasi LS={result['lokasi']['ls']}, BT={result['lokasi']['bt']}")
-    print(f"Pusat_gempa' {result['pusat_gempa']}")
-    print(f"Keterangan' {result['keterangan']}")
+    print(f"Tanggal {result['tanggal']}")
+    print(f"Waktu {result['waktu']}")
+    print(f"Magnitudo {result['magnitudo']}")
+    print(f"Kedalaman {result['kedalaman']}")
+    print(f"Lokasi : LS={result['lokasi']['ls']}, BT={result['lokasi']['bt']}")
+    print(f"Pusat_gempa {result['pusat_gempa']}")
+    print(f"Keterangan {result['keterangan']}")
 
 
 if __name__ == '__main__':
